eadc2147de23522c9f29828ce42d9277197c411b
novitaguok <novitaguok@gmail.com>
2026-05-18 13:48:44 +0700

    First commit

---
+++ sharedLogic/src/commonMain/kotlin/com/owlite/socialexit/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.owlite.socialexit
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ sharedLogic/src/commonMain/kotlin/com/owlite/socialexit/Platform.kt
@@ -0,0 +1,7 @@
+package com.owlite.socialexit
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
